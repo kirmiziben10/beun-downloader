9fdc2d6b07cfbf03dfba8334227f2d44993c2c53
kirmiziben10 <76447549+kirmiziben10@users.noreply.github.com>
2021-12-30 15:20:21 +0300

    Feel_Better.exe
    Cleaned up a lot of the junk there are still some bad left in this code tho.
    You no longer need to paste cookies to use the program name and pass works great.

--- downloader.py
+++ downloader.py
@@ -1,21 +1,22 @@
 import requests
-import urllib.request
-import json
-import urllib.request
-import time
+import os
 
-#You need to go to your class and right click copy the videos link. Copying the url from the top of the video works too.
-#There will still be failed downloads i couldn't get the request to json to work properly so i did whatever i can to make it work most of the time.
+username = ''#Your UZEM Username.
+password = ''#Your UZEM Password.
 
-#You need to get these forum your browsers cookiejar. (Developer console -> applicaiton tab -> cookies (For Chrome))
-verification = ''
-sessionid = ''
-auth = ''
+session = requests.session()
 
-#Copied form stack overflow .d
+def clear():
+    os.system('cls' if os.name=='nt' else 'clear')
+
+def getCookies():
+    session.post('https://ue.beun.edu.tr:443/', verify=False)
+    session.cookies.set('CookUserName', username)
+    session.post('https://ue.beun.edu.tr:443/', verify=False, data= {'Password': password})
+
 def readFile(fileName):
-        fileObj = open(fileName, "r") #opens the file in read mode
-        words = fileObj.read().splitlines() #puts the file into an array
+        fileObj = open(fileName, "r") #Opens the file in read mode.
+        words = fileObj.read().splitlines() #Puts the file into an array.
         fileObj.close()
         return words
 
@@ -24,34 +25,47 @@ def parseUrl(url):
 
 def parseUrls():
     ids= []
-    for i in readFile('urls.txt'):
-        ids.append(parseUrl(i))
+    for i in readFile('urls.txt'): #Reads the urls from txt.
+        if i[0] == '#': #Comment check.
+            continue
+        else:
+            ids.append(parseUrl(i)) #Adds the urls into an array.
     print(ids)
     return ids
 
-#Uses requests and cookies to get videos. Very rude of them to make it this hard. Very secure tho.
 def getVideoUrl(id):
     url= 'https://ue.beun.edu.tr/Video/ManageInteraction?id=' + id
 
-    r = requests.post(url, verify=False, data = {'id': id}, cookies = {'__RequestVerificationToken':verification, 'ASP.NET_SessionId':sessionid, '.ASPXAUTH':auth})
-    if r.text[154] == "/": #Whatever i can to make it work most of the time.
+    r = session.post(url, verify=False, data = {'id': id})
+    print('Got the direct download of: ' + id)
+
+    #I hate myself for doing this. Please fix this if you are reading.
+    #Variations of some url types. Works for all of my 23 videos.
+    if r.text[154] == '/':
         return r.text[154:-47]
+    elif r.text[153] == '/':
+        return r.text[153:-47]
     else:
         return r.text[155:-47]
 
 def assembleDownloads(headlessUrl):
     return 'https://ue.beun.edu.tr'+ headlessUrl
 
-#Gets the parsed urls and Downloads them.
 def main():
+    print('Getting the cookies.')
+    getCookies()
     links = []
     for i in parseUrls():
-        read = requests.get(assembleDownloads(getVideoUrl(i)), verify=False, cookies={'__RequestVerificationToken':verification, 'ASP.NET_SessionId':sessionid, '.ASPXAUTH':auth})
-        w = open(i + '.mp4', 'wb')
-        time.sleep(1)
-        for chunk in read.iter_content(chunk_size=4096):
+        read = session.get(assembleDownloads(getVideoUrl(i)), verify=False) #"reads" the video from the server
+        print('Starting the download.')
+        w = open(i + '.mp4', 'wb') #Creates or opens the video
+        x = 0
+        loading ='|\\-/'
+        clear()
+        for chunk in read.iter_content(chunk_size=512): #Divides data into chunks for ram i guess i stole this from the internet
             if chunk:
                 w.write(chunk)
-            print(i + ' downloaded successfully!!!')
+            print('downloading '+  i + loading[x % 4])
+            x+=1
 
 main()
